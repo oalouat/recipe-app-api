4cbee6545eeed9975a3e859bdfa9350fb1ea5046
oalouat <learnonudemy@yahoo.com>
2025-07-30 10:59:03 +0400

    FIX: Add the option to get a recipe detail

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions, authentication
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 from core.models import Recipe
 from rest_framework import viewsets
 
@@ -8,7 +8,7 @@ from rest_framework import viewsets
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs"""
 
-    serializer_class = RecipeSerializer
+    serializer_class = RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes=[permissions.IsAuthenticated]
@@ -16,3 +16,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+    
+
+    def get_serializer_class(self):
+        """Return the serializer class for request"""
+        if self.action == 'list':
+            return RecipeSerializer
+        
+        return self.serializer_class
+        

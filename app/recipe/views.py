76a5885bb8701da67289e9021e97e2a4b55843b8
oalouat <learnonudemy@yahoo.com>
2025-07-30 10:24:45 +0400

    FEAT: add recipe implentation

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import permissions, authentication
 
-# Create your views here.
+from recipe.serializers import RecipeSerializer
+from core.models import Recipe
+from rest_framework import viewsets
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs"""
+
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes=[permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
